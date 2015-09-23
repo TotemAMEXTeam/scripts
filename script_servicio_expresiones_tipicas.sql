d536b3d26cd3f6f872b53899905defe17e152433
Sandra <sandraebro@gmail.com>
2015-09-23 18:38:25 +0200

    Cambios servicio expresiones tipicas

--- script_servicio_expresiones_tipicas.sql
+++ script_servicio_expresiones_tipicas.sql
@@ -1,7 +1,8 @@
 CREATE TABLE language (
   code VARCHAR(2) NOT NULL COMMENT 'ISO 639-1 language code',
   language VARCHAR(45) NOT NULL COMMENT 'Language name, in english',
-  PRIMARY KEY (code)  COMMENT 'Table primary key');
+  PRIMARY KEY (code)  COMMENT 'Table primary key')
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
   
 INSERT INTO language (code, language) values ('es', 'Spanish'); 
 INSERT INTO language (code, language) values ('en', 'English'); 
@@ -14,11 +15,12 @@ INSERT INTO language (code, language) values ('ar', 'Arabic');
 
 CREATE TABLE language_expression (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Numeric autoincremental id',
-  origin_language VARCHAR(2) NOT NULL COMMENT 'Language code origin',
-  destiny_language VARCHAR(2) NOT NULL COMMENT 'Language code destiny',
-  origin_expression VARCHAR(255) NOT NULL COMMENT 'Expression in original language',
-  destiny_expression VARCHAR(255) NOT NULL COMMENT 'Expression in destiny language',
-  explanation VARCHAR(255) COMMENT 'Expression explanation');
+  origin_language VARCHAR(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Language code origin',
+  destiny_language VARCHAR(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Language code destiny',
+  origin_expression VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Expression in original language',
+  destiny_expression VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Expression in destiny language',
+  explanation VARCHAR(255)  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT 'Expression explanation')
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE language_expression ADD CONSTRAINT fk_origin_language FOREIGN KEY (origin_language) REFERENCES language(code) ON DELETE CASCADE;
 ALTER TABLE language_expression ADD CONSTRAINT fk_destiny_language FOREIGN KEY (destiny_language) REFERENCES language(code) ON DELETE CASCADE;
@@ -28,3 +30,9 @@ CREATE INDEX i_destiny_language ON language_expression (destiny_language);
 
 INSERT INTO language_expression (origin_language, destiny_language, origin_expression, destiny_expression, explanation)
 	VALUES ('es','en','¡Salud!','Cheers!','Para celebrar un brindis');
+INSERT INTO language_expression (origin_language, destiny_language, origin_expression, destiny_expression, explanation)
+	VALUES ('es','en','¡Guay!','Cool!','Para indicar agrado sobre algo');
+INSERT INTO language_expression (origin_language, destiny_language, origin_expression, destiny_expression, explanation)
+	VALUES ('es','fr','Es lo que hay','C\'est la vie','Para indicar que hay que resignarse cuando ocurre algo');
+INSERT INTO language_expression (origin_language, destiny_language, origin_expression, destiny_expression, explanation)
+	VALUES ('es','fr','A mogollón','A gogo','Para expresar una cantidad enorme de algo');
